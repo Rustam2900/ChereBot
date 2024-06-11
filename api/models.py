113bc6e8e77c09c62734e7c5050c2ec22f2015e3
Rustam2900 <139065064+Rustam2900@users.noreply.github.com>
2024-06-11 16:21:54 +1000

    modellarni o'zgartirdik

--- api/models.py
+++ api/models.py
@@ -1,23 +1,37 @@
 from django.db import models
 
 
+class LanguageCodes(models.TextChoices):
+    EN = 'en', 'English'
+    UZ = 'uz', 'Uzbek'
+    RU = 'ru', 'Russian'
+
+
 class BotUser(models.Model):
-    user_id = models.CharField(max_length=100)
+    telegram_id = models.BigIntegerField(max_length=100, unique=True)
     name = models.CharField(max_length=100)
     username = models.CharField(max_length=100, null=True, blank=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
     create_at = models.DateTimeField(auto_now_add=True)
+    language = models.CharField(
+        max_length=2,
+        choices=LanguageCodes.choices,
+        default=LanguageCodes.UZ
+    )
 
     def __str__(self):
         return self.name
 
 
-class UserWater(models.Model):
-    title = models.CharField(max_length=100, null=True, blank=True)
-    orders = models.CharField(max_length=100)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    create_at = models.DateTimeField(auto_now_add=True)
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.IntegerField()
 
     def __str__(self):
-        return self.title
+        return self.description
 
+
+class Order(models.Model):
+    pass
