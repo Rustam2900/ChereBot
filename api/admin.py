9747414938866d385a418125ef5650c317bdbefb
Rustam2900 <139065064+Rustam2900@users.noreply.github.com>
2024-06-11 17:09:18 +1000

    models update

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from api.models import BotUser
+from api.models import BotUser, Product, Order
 
 admin.site.register(BotUser)
+admin.site.register(Product)
+admin.site.register(Order)
