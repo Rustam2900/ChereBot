6f6c67d88e8b306ff7ce016952824e9750948a75
Turon Kuzibaev <kuzibaevturonbek@gmail.com>
2024-07-06 17:38:28 +0500

    user registration statesgroup

--- bot/filters/states.py
+++ bot/filters/states.py
@@ -2,5 +2,9 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class Registration(StatesGroup):
+    user_type = State()
+    phone_number = State()
     first_name = State()
     last_name = State()
+
+    ## extra fields
