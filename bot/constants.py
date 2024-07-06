9fd3ac1061cb725e7cfab58772d66de08903ac4b
Turon Kuzibaev <kuzibaevturonbek@gmail.com>
2024-07-06 18:35:10 +0500

    constants

--- bot/constants.py
+++ bot/constants.py
@@ -9,3 +9,11 @@ CHOOSE_LANGUAGE = (
 CANCEL = _("❌ Cancel")
 
 ASK_WHO_IS_THIS_PERSON = _("Are you an individual or a legal entity?")
+
+INDIVIDUAL = _("Individual")
+LEGAL_ENTITY = _("Legal entity")
+
+COMMON_ERROR_TEXT = _("An error occurred. Try again later.")
+
+REQUEST_CONTACT_TEXT = _("Send your phone number to register\n"
+                         "Send the number in this format +998*********")
