66f0beccfe9efb1f675bfd3aabc0058fe4e51b2c
Turon Kuzibaev <kuzibaevturonbek@gmail.com>
2024-07-06 17:38:48 +0500

    constants changes

--- bot/constants.py
+++ bot/constants.py
@@ -1,6 +1,11 @@
+from django.utils.translation import gettext_lazy as _
 
 CHOOSE_LANGUAGE = (
     "➖ Tilni tanlang!\n"
     "➖ Выберите язык!\n"
     "➖ Choose a language!\n"
 )
+
+CANCEL = _("❌ Cancel")
+
+ASK_WHO_IS_THIS_PERSON = _("Are you an individual or a legal entity?")
